4a89b0ca9acc5ad1110ea37b40f358cfedd9c94b
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-05-12 00:48:05 +0900

    Init: Tailwind 설치 및 Global Style 초기 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,14 +6,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -28,6 +18,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="bg-green-200 text-center text-xl font-bold p-4 rounded-md">
+        <p> ✅ Tailwind 적용 테스트</p>
+    </div>
     </>
   )
 }
