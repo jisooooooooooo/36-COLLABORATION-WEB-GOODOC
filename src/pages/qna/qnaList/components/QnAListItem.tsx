6d7adaf44e0d6f4cf668ac25bc6c930595d93d06
jstar_00 <wltjd6300@naver.com>
2025-05-15 16:42:44 +0900

    fix: defined but never used 오류 해결

--- src/pages/qna/qnaList/components/QnAListItem.tsx
+++ src/pages/qna/qnaList/components/QnAListItem.tsx
@@ -11,15 +11,13 @@ const QnAListItem = ({ question, summary, department, timeElapsed }: QnAListItem
   return (
     <div className="flex flex-col w-[20.9375rem]">
       <div className="flex gap-[.625rem] items-center text-CGray-4">
-        <span className="caption-semi-13">내과</span>
+        <span className="caption-semi-13">{department}</span>
         <Ellipsis />
-        <span className="caption-reg-13">22시간 전</span>
+        <span className="caption-reg-13">{timeElapsed}</span>
       </div>
 
-      <span className="title-semi-18 mt-[.625rem] line-clamp-2">위고비를 맞아도 되나요?</span>
-      <span className="body-reg-14 mt-[.5rem] text-CGray-4 line-clamp-2">
-        당뇨약을 약하게 먹고 있는데 위고비를 맞아도 될까요?
-      </span>
+      <span className="title-semi-18 mt-[.625rem] line-clamp-2">{question}</span>
+      <span className="body-reg-14 mt-[.5rem] text-CGray-4 line-clamp-2">{summary}</span>
     </div>
   );
 };
