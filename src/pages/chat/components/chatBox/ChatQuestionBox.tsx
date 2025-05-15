1abc4ae8ac026f06cc6a06b5ca093afc5e8664fd
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-05-16 02:30:03 +0900

    refactor: px -> rem으로 수정

--- src/pages/chat/components/chatBox/ChatQuestionBox.tsx
+++ src/pages/chat/components/chatBox/ChatQuestionBox.tsx
@@ -9,7 +9,7 @@ const ChatWelcomeBox = () => {
             상담이 필요한 내용을 알려주세요. <span className="text-CGray-4">(1/3)</span>
           </p>
           <p className="body-med-14 text-CGray-4">예)</p>
-          <ul className="body-med-14 text-CGray-4 list-disc pl-[19px] space-y-[0.25rem]">
+          <ul className="body-med-14 text-CGray-4 list-disc pl-[1.1875rem] space-y-[0.25rem]">
             <li>혈압약을 먹고 운동해도 될까요?</li>
             <li>손발이 자주 저리는데 이유가 뭘까요?</li>
             <li>피부 두드러기가 났어요 어떻게 해야하죠?</li>
