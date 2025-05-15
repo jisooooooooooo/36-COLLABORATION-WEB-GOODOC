892090b8d62935ee4846d35bfdf088d6fb29e0fa
mmaybei <yebbi2@ewhain.net>
2025-05-16 02:51:45 +0900

    feat: 메인 뷰 병원 더보기 버튼 구현

---
+++ src/pages/main/components/MoreButton.tsx
@@ -0,0 +1,7 @@
+export default function MoreButton() {
+  return (
+    <button className="w-full h-[3rem] rounded-[.5rem] border-none bg-CGray-8">
+      <div className="title-semi-16 text-CGray-3">병원 더보기</div>
+    </button>
+  );
+}
