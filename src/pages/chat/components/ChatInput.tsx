b7f6a6a181ef5b4fcb500704f9b2daf98a5a04d3
jisooooooooooo <dlawltn0708@naver.com>
2025-05-16 02:53:23 +0900

    feat: 입력창 컴포넌트 배경색 추가

--- src/pages/chat/components/ChatInput.tsx
+++ src/pages/chat/components/ChatInput.tsx
@@ -2,13 +2,15 @@ import SendIcon from '@shared/assets/svg/send.svg?react';
 
 const ChatInput: React.FC = () => {
   return (
-    <div className="flex items-center mb-[2.125rem] px-[1rem] py-[.75rem] rounded-tl-[.5rem] rounded-tr-[.5rem] bg-white shadow-[.25rem_-0.25rem_1.25rem_0rem_rgba(0,0,0,0.05)]">
-      <input
-        type="text"
-        placeholder="메시지를 입력하세요."
-        className="flex-1 h-[2.5rem] px-[.625rem] py-[.5625rem] border border-[#ECEDF0] rounded-[.75rem] bg-white body-med-14 placeholder:body-med-14 placeholder:text-CGray-6 outline-none"
-      />
-      <SendIcon className="w-[1.5rem] h-[1.5rem] ml-[.75rem] cursor-pointer text-CGray-6" />
+    <div className="bg-CGray-8">
+      <div className="flex items-center px-[0.75rem] pt-[.5rem] pb-[2.625rem] bg-White rounded-tl-[.5rem] rounded-tr-[.5rem] shadow-[.25rem_-0.25rem_1.25rem_0rem_rgba(0,0,0,0.05)]">
+        <input
+          type="text"
+          placeholder="메시지를 입력하세요."
+          className="flex-1 h-[2.5rem] px-[.625rem] py-[.5625rem] border border-[#ECEDF0] rounded-[.75rem] bg-white body-med-14 placeholder:body-med-14 placeholder:text-CGray-6 outline-none"
+        />
+        <SendIcon className="w-[1.5rem] h-[1.5rem] ml-[.75rem] cursor-pointer text-CGray-6" />
+      </div>
     </div>
   );
 };
