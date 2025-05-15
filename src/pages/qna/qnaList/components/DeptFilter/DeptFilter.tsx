5a8aa550234a58194378892f16c5463f103738df
jstar_00 <wltjd6300@naver.com>
2025-05-15 22:24:41 +0900

    refactor: DeptFilter.tsx의 상태관리 코드를 커스텀 훅으로 분리(useDeptFilter.ts)

--- src/pages/qna/qnaList/components/DeptFilter/DeptFilter.tsx
+++ src/pages/qna/qnaList/components/DeptFilter/DeptFilter.tsx
@@ -1,23 +1,14 @@
 import Chevron from '@shared/assets/svg/chevron-updown.svg?react';
 import DeptBox from './DeptBox';
 import { deptList } from '@/shared/constants/constant';
-import { useState } from 'react';
+import { useDeptFilter } from '../hook/useDeptFilter';
 
 const DeptFilter = () => {
-  const [showDeptList, setShowDeptList] = useState(false);
-  const [selectedDept, setSelectedDept] = useState<string | null>(null);
-
-  const handleDeptListClick = () => {
-    setShowDeptList(!showDeptList);
-  };
-
-  const handleDeptBoxClick = (deptName: string) => {
-    setSelectedDept(deptName);
-  };
+  const { showDeptList, selectedDept, toggleDeptList, selectDept } = useDeptFilter();
 
   return (
     <div className="flex flex-col w-full ml-[20px] mt-[20px]">
-      <button type="button" className="flex" onClick={handleDeptListClick}>
+      <button type="button" className="flex" onClick={toggleDeptList}>
         <span className="title-bold-20 text-CGray-1 mr-[.25rem]">진료과 전체</span>
         <Chevron
           className={`transition-transform duration-300 ${!showDeptList ? 'rotate-180' : 'rotate-0'}`}
@@ -33,7 +24,7 @@ const DeptFilter = () => {
                 key={index}
                 deptName={dept}
                 isSelected={dept === selectedDept}
-                onBoxClick={() => handleDeptBoxClick(dept)}
+                onBoxClick={() => selectDept(dept)}
               />
             ))}
           </div>
