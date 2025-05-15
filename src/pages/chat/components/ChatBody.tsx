ebb21f651d10c00d2754e527d79e188aaca2979d
jisooooooooooo <dlawltn0708@naver.com>
2025-05-16 02:55:56 +0900

    fix: 단위 변경

--- src/pages/chat/components/ChatBody.tsx
+++ src/pages/chat/components/ChatBody.tsx
@@ -4,8 +4,8 @@ const NOTICE_MESSAGE = '익명으로 공개하니 안심하세요';
 
 const ChatBody: React.FC = () => {
   return (
-    <section className="bg-CGray-8 h-full flex flex-col pb-20">
-      <div className="flex items-center py-[0.94rem] pl-[1rem] gap-[0.25rem]">
+    <section className="bg-CGray-8 h-full flex flex-col pb-[5rem]">
+      <div className="flex items-center py-[.94rem] pl-[1rem] gap-[.25rem]">
         <Alert className="w-[1rem] h-[1rem]" aria-hidden="true" />
         <p className="title-semi-14 text-Mainblue">{NOTICE_MESSAGE}</p>
       </div>
