a4c795bef2cc74b1c7b2758d14fa45dfed97e4b8
mmaybei <yebbi2@ewhain.net>
2025-05-16 02:52:16 +0900

    feat: 메뉴 뷰 로그인 및 다운로드 버튼 구현

---
+++ src/pages/menu/components/DownloadButton.tsx
@@ -0,0 +1,7 @@
+export default function DownladButton() {
+  return (
+    <button className="w-full h-[3rem] rounded-[.5rem] border-none bg-Blue-3">
+      <div className="title-semi-16 text-Mainblue">앱 다운로드</div>
+    </button>
+  );
+}
