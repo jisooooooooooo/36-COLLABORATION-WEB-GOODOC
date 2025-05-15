1c68768b5808dcff921e13c6bb3ec1a05a73c3db
jstar_00 <wltjd6300@naver.com>
2025-05-15 16:14:32 +0900

    fix: 글자색 적용

--- src/pages/qna/qnaList/components/QnAListItem.tsx
+++ src/pages/qna/qnaList/components/QnAListItem.tsx
@@ -10,14 +10,14 @@ interface QnAListItemProps {
 const QnAListItem = ({ question, summary, department, timeElapsed }: QnAListItemProps) => {
   return (
     <div className="flex flex-col w-[20.9375rem]">
-      <div className="flex gap-[.625rem] items-center">
+      <div className="flex gap-[.625rem] items-center text-CGray-4">
         <span className="caption-semi-13">내과</span>
         <Ellipsis />
         <span className="caption-reg-13">22시간 전</span>
       </div>
 
       <span className="title-semi-18 mt-[.625rem] line-clamp-2">위고비를 맞아도 되나요?</span>
-      <span className="body-reg-14 mt-[.5rem] line-clamp-2">
+      <span className="body-reg-14 mt-[.5rem] text-CGray-4 line-clamp-2">
         당뇨약을 약하게 먹고 있는데 위고비를 맞아도 될까요?
       </span>
     </div>
