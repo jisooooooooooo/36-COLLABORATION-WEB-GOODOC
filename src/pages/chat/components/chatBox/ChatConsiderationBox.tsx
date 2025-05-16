dbd76ad05cd127e0f7caba9cf41954738af11097
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-05-16 21:32:37 +0900

    feat: 채팅 뷰 3번째 챗봇 박스 구현

---
+++ src/pages/chat/components/chatBox/ChatConsiderationBox.tsx
@@ -0,0 +1,34 @@
+import { useSelectableOption } from '@pages/chat/hooks/useSelectableOption';
+import ChatBubbleBot from '@pages/chat/components/chatBot/ChatBubbleBot';
+import SelectableButton from '@pages/chat/components/chatBot/SelectableButton';
+
+const options = ['없음', '임신 가능성', '복약중인 약', '주의할 약', '만성질환', '기타'];
+
+const ChatConsiderationBox = () => {
+  const { setSelectedLabel, isSelected } = useSelectableOption('없음');
+
+  return (
+    <ChatBubbleBot
+      message={
+        <div className="flex flex-col gap-[1rem]">
+          <p className="body-med-14 text-CGray-2 break-keep">
+            이 중 제가 고려해야 할 정보가 있을까요?<span className="text-CGray-4">(2/3)</span>
+          </p>
+          <div className="grid grid-cols-2 gap-[.5rem] p-[.25rem]">
+            {options.map(label => (
+              <SelectableButton
+                key={label}
+                label={label}
+                isSelected={isSelected(label)}
+                onClick={() => setSelectedLabel(label)}
+              />
+            ))}
+          </div>
+        </div>
+      }
+      time="오후 10:20"
+    />
+  );
+};
+
+export default ChatConsiderationBox;
