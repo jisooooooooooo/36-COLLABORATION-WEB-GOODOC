b74c13f2c8541ffe677fc69f5db5c4306cae3323
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-05-16 21:20:17 +0900

    design: 챗 메시지 컴포넌트 mb 속성 추가

--- src/pages/chat/components/chatBot/ChatBubbleBot.tsx
+++ src/pages/chat/components/chatBot/ChatBubbleBot.tsx
@@ -8,7 +8,7 @@ interface ChatBubbleBotProps {
 const ChatBubbleBot: React.FC<ChatBubbleBotProps> = ({ message, time }) => {
   return (
     <div className="flex justify-center w-full">
-      <div className="w-[20.9375rem] h-[14.0625rem] flex items-start gap-[.75rem]">
+      <div className="w-[20.9375rem] flex items-start gap-[.75rem] mb-[1.5rem]">
         <ChatProfileIcon className="w-[2.25rem] h-[2.25rem] min-w-[2.25rem] min-h-[2.25rem] block" />
         <div className="flex flex-col">
           <div className="bg-White w-[16.875rem] text-CGray-2 p-[.75rem] rounded-tr-[.75rem] rounded-br-[.75rem] rounded-bl-[.75rem]">
