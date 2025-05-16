b0a1b6dcc3c3beec8b3204b127546f25523cd3ae
jisooooooooooo <dlawltn0708@naver.com>
2025-05-16 19:31:14 +0900

    feat: 상담 첫번째 채팅 구현

--- src/pages/chat/components/ChatBody.tsx
+++ src/pages/chat/components/ChatBody.tsx
@@ -1,27 +1,47 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Alert from '@shared/assets/svg/chatAlert.svg?react';
-import ChatWelcomeBox from '@/pages/chat/components/chatBox/ChatWelcomeBox';
-import ChatUser from './user/ChatUser';
 import { formatTime } from '@/shared/utils/date';
+import ChatWelcomeBox from '@/pages/chat/components/chatBox/ChatWelcomeBox';
+import ChatQuestionBox from './chatBox/ChatQuestionBox';
+import ChatUser from './user/ChatUser';
 
 const NOTICE_MESSAGE = '익명으로 공개하니 안심하세요';
 
 const ChatBody: React.FC = () => {
-  const [userStarted, setUserStarted] = useState(false);
+  const [chatStep, setChatStep] = useState<'initial' | 'started'>('initial');
+
+  const userStartTimeRef = useRef<string | null>(null);
+  const scrollRef = useRef<HTMLDivElement | null>(null);
 
   const handleStart = () => {
-    setUserStarted(true);
+    userStartTimeRef.current = formatTime(new Date());
+    setChatStep('started');
   };
 
+  const getUserStartTime = () => userStartTimeRef.current ?? formatTime(new Date());
+
+  useEffect(() => {
+    if (chatStep === 'started' && scrollRef.current) {
+      scrollRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [chatStep]);
+
   return (
-    <section className="bg-CGray-8 h-full flex flex-col pb-[5rem] gap-[1.5rem]">
+    <section className="bg-CGray-8 min-h-full flex flex-col pt-[3.25rem] pb-[5rem] gap-[1.5rem]">
       <div className="flex items-center py-[.94rem] pl-[1rem] gap-[.25rem]">
         <Alert className="w-[1rem] h-[1rem]" aria-hidden="true" />
         <p className="title-semi-14 text-Mainblue">{NOTICE_MESSAGE}</p>
       </div>
 
       <ChatWelcomeBox onStart={handleStart} />
-      {userStarted && <ChatUser message="상담 시작하기" time={formatTime(new Date())} />}
+
+      {chatStep === 'started' && (
+        <>
+          <ChatUser message="상담 시작하기" time={getUserStartTime()} />
+          <ChatQuestionBox />
+          <div ref={scrollRef} />
+        </>
+      )}
     </section>
   );
 };
