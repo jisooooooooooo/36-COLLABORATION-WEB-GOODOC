78326d0addaba1ca54cf733618559b3f39b3de46
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-05-16 21:21:23 +0900

    feat: chat body에 각 메시지 박스 컴포넌트 추가

--- src/pages/chat/components/ChatBody.tsx
+++ src/pages/chat/components/ChatBody.tsx
@@ -1,4 +1,7 @@
 import Alert from '@shared/assets/svg/chatAlert.svg?react';
+import ChatConsiderationBox from '@pages/chat/components/chatBox/ChatConsiderationBox';
+import ChatQuestionBox from '@pages/chat/components/chatBox/ChatQuestionBox';
+import ChatWelcomeBox from '@pages/chat/components/chatBox/ChatWelcomeBox';
 
 const NOTICE_MESSAGE = '익명으로 공개하니 안심하세요';
 
@@ -8,6 +11,11 @@ const ChatBody: React.FC = () => {
       <div className="flex items-center py-[.94rem] pl-[1rem] gap-[.25rem]">
         <Alert className="w-[1rem] h-[1rem]" aria-hidden="true" />
         <p className="title-semi-14 text-Mainblue">{NOTICE_MESSAGE}</p>
+      </div>
+      <div>
+        <ChatWelcomeBox />
+        <ChatConsiderationBox />
+        <ChatQuestionBox />
       </div>
     </section>
   );
