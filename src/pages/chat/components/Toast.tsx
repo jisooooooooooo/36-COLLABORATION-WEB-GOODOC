823b36cebfabbf3c4c3d887db6beab9df57fb13b
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-05-19 19:38:23 +0900

    design: 코드 리뷰 반영(스타일 속성 수정)

--- src/pages/chat/components/Toast.tsx
+++ src/pages/chat/components/Toast.tsx
@@ -14,7 +14,7 @@ const Toast: React.FC<ToastProps> = ({ message, onClose, duration = 3000 }) => {
   }, [onClose, duration]);
 
   return (
-    <div className="absolute left-1/2 -translate-x-1/2 top-[3.875rem] inline-flex items-center fixed gap-[0.4rem] bg-SoftRed body-med-16 text-ErrorRed px-[0.875rem] py-[1.1rem] rounded-[0.88rem] z-3 whitespace-nowrap">
+    <div className="left-1/2 -translate-x-1/2 top-[3.875rem] inline-flex items-center fixed gap-[0.4rem] bg-SoftRed body-med-16 text-ErrorRed px-[0.875rem] py-[1.1rem] rounded-[0.88rem] z-3 whitespace-nowrap">
       <ErrorToast className="w-[0.875rem] h-[0.875rem] shrink-0" />
       <span>{message}</span>
     </div>
