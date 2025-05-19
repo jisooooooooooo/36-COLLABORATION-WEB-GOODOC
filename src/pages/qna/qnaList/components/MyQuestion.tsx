fed03fefce2950c8194c12dd7b9957190731368b
jstar_00 <wltjd6300@naver.com>
2025-05-19 20:26:50 +0900

    feat: 뒤로가기, 내 질문 navigate 구현

--- src/pages/qna/qnaList/components/MyQuestion.tsx
+++ src/pages/qna/qnaList/components/MyQuestion.tsx
@@ -1,10 +1,18 @@
 import ForwardArrow from '@shared/assets/svg/IconForwardArrow.svg?react';
+import { useNavigate } from 'react-router';
 
 const MyQuestion = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/qna/my');
+  };
+
   return (
     <button
       type="button"
       className="flex justify-between items-end w-[20.9375rem] mt-[1.25rem] mb-[2.5rem] py-[1.6875rem] px-[1.5rem] border border-[#ECEDF0] rounded-[.75rem]"
+      onClick={handleClick}
     >
       <span className="title-semi-18 text-CGray">내 질문</span>
       <div className="flex  gap-[.25rem] items-end">
--- src/pages/qna/qnaMy/page/QnAMy.tsx
+++ src/pages/qna/qnaMy/page/QnAMy.tsx
@@ -1,5 +1,9 @@
 const QnAMy = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col mt-[36px]">
+      <span className="title-bold-20 self-start">내 질문</span>
+    </div>
+  );
 };
 
 export default QnAMy;
