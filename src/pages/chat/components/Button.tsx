6047804d8481b8655fd8720e5d2c6d91405f7849
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-05-20 16:57:56 +0900

    design: 버튼 컴포넌트 컬러 추가

--- src/pages/chat/components/Button.tsx
+++ src/pages/chat/components/Button.tsx
@@ -1,11 +1,17 @@
 interface ButtonProps {
   label: string;
   onClick: () => void;
-  variant?: 'primary' | 'secondary';
+  variant?: 'primary' | 'secondary' | 'gray';
 }
 
 const Button: React.FC<ButtonProps> = ({ label, onClick, variant = 'primary' }) => {
-  const buttonStyles = variant === 'primary' ? 'bg-Blue-3 text-Mainblue' : 'bg-Mainblue text-White';
+  const buttonStyles =
+    variant === 'primary'
+      ? 'bg-Blue-3 text-Mainblue'
+      : variant === 'secondary'
+        ? 'bg-Mainblue text-White'
+        : 'bg-WGray-2 text-WGray-1';
+
   return (
     <button
       onClick={onClick}
