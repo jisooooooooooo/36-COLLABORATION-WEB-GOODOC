a44aadc7821b91413dd971acba95e97237f8f359
jisooooooooooo <dlawltn0708@naver.com>
2025-05-20 12:37:48 +0900

    feat: 애니메이션 추가

--- src/pages/chat/components/chatBox/ChatBubbleBot.tsx
+++ src/pages/chat/components/chatBox/ChatBubbleBot.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from 'react';
 import ChatProfileIcon from '@shared/assets/svg/chatProfile.svg?react';
+import { chatFadeUp, chatEnter, chatHidden } from '@/shared/styles/animation';
 
 interface ChatBubbleBotProps {
   message: React.ReactNode;
@@ -6,9 +8,18 @@ interface ChatBubbleBotProps {
 }
 
 const ChatBubbleBot: React.FC<ChatBubbleBotProps> = ({ message, time }) => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setVisible(true), 100);
+    return () => clearTimeout(timer);
+  }, []);
+
+  const animationClass = visible ? chatEnter : chatHidden;
+
   return (
-    <div className="flex justify-center w-full">
-      <div className="w-[20.9375rem] h-[auto] flex items-start gap-[.75rem]">
+    <div className={`flex justify-center w-full ${chatFadeUp} ${animationClass}`}>
+      <div className="w-[20.9375rem] h-auto flex items-start gap-[.75rem]">
         <ChatProfileIcon className="w-[2.25rem] h-[2.25rem] min-w-[2.25rem] min-h-[2.25rem] block" />
         <div className="flex flex-col">
           <div className="bg-White w-[16.875rem] text-CGray-2 p-[.75rem] rounded-tr-[.75rem] rounded-br-[.75rem] rounded-bl-[.75rem]">
