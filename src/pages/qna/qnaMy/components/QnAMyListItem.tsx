da5e490457711e7693836f2a01f3d28bfbe5a8e8
jstar_00 <wltjd6300@naver.com>
2025-05-20 14:45:08 +0900

    feat: QnALayout에 상단 PromotionBanner 추가, '내 질문' 페이지 ListItem 추가

---
+++ src/pages/qna/qnaMy/components/QnAMyListItem.tsx
@@ -0,0 +1,26 @@
+import Ellipsis from '@shared/assets/svg/ellipsis.svg?react';
+
+interface QnAHeaderProps {
+  department: string;
+  title: string;
+  keyword: string;
+  date: string;
+}
+
+const QnAMyListItem = ({ department, title, keyword, date }: QnAHeaderProps) => {
+  return (
+    <div>
+      <article className="w-[20.9375rem] mb-[3rem]">
+        <header className="mb-[1rem] flex items-center gap-[0.5rem]">
+          <span className="caption-semi-13 text-CGray-4">{department}</span>
+          <Ellipsis className="h-[0.125rem] w-[0.125rem]" />
+          <span className="caption-reg-13 text-CGray-4">{date}</span>
+        </header>
+        <div className="mb-[.5rem] title-semi-18 text-CGray-1">{title}</div>
+        <div className="body-reg-14 text-CGray-4">{keyword}</div>
+      </article>
+    </div>
+  );
+};
+
+export default QnAMyListItem;
